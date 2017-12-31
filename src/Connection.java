1ce711582001ecf63365761e1c8f3f5dfa53439b
Marek <mpietrewicz@gmail.com>
2017-12-31 13:03:45 +0100

    Dodanie walidacji portu (id hosta) i przeniesienie tworzenia Socketa do konstruktora klasy Connection

--- src/Connection.java
+++ src/Connection.java
@@ -3,18 +3,36 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.rmi.server.ExportException;
 
 public class Connection {
-    private Socket clientSocket;
+    static int portStartNumber = 10000;
+    static int portEndNumber = 10100;
+
+    private Socket socket;
     private InputStreamReader inputStream;
     private BufferedReader inputStreamBufferedReader;
     private DataOutputStream outputStream;
 
-    public Connection(Socket clientSocket) throws IOException {
-        this.clientSocket = clientSocket;
-        this.outputStream = new DataOutputStream(clientSocket.getOutputStream());
-        this.inputStream = new InputStreamReader(clientSocket.getInputStream());
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        this.outputStream = new DataOutputStream(socket.getOutputStream());
+        this.inputStream = new InputStreamReader(socket.getInputStream());
         this.inputStreamBufferedReader = new BufferedReader(inputStream);
+    }
+
+    public Connection(String ip, int port) throws IOException {
+        if(!isPortValid(port)) {
+            throw new ExportException("Port number must be in range: 10001-10100");
+        }
+        this.socket = new Socket(ip, port);
+        this.outputStream = new DataOutputStream(socket.getOutputStream());
+        this.inputStream = new InputStreamReader(socket.getInputStream());
+        this.inputStreamBufferedReader = new BufferedReader(inputStream);
+    }
+
+    public Connection(String ip, String port) throws IOException {
+        this(ip, Integer.parseInt(port));
     }
 
     public void sendMessage(String message) throws IOException {
@@ -25,4 +43,16 @@ public class Connection {
         return inputStreamBufferedReader.readLine();
     }
 
+    public static boolean isPortValid(int port) {
+        if(port > portStartNumber && port<portEndNumber) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static boolean isPortValid(String port) {
+        return isPortValid(Integer.parseInt(port));
+    }
 }
--- src/Host.java
+++ src/Host.java
@@ -10,8 +10,9 @@ public class Host {
         Socket clientSocket;
         Connection connection;
         String response;
+        String port;
 
-        if(id > 0 && id<100) {
+        if(Connection.isPortValid(10000+id)) {
             System.out.println("Starting host: "+id);
         }
         else {
@@ -32,8 +33,7 @@ public class Host {
                     System.out.println("Wymiana list udostępnianych plików między hostami " +
                             "-  chcemy wiedzieć gdzie (na jakim hoście) jakie pliki się znajdują, wraz z ich sumami kontrolnymi MD5");
 
-                    clientSocket = new Socket("127.0.0.1", 10000);
-                    connection = new Connection(clientSocket);
+                    connection = new Connection("127.0.0.1", 10000);
                     connection.sendMessage(command);
                     response = connection.readMessage();
                     System.out.println(response);
@@ -42,18 +42,31 @@ public class Host {
                 case "SET":
                     System.out.println("Wymiana list udostępnianych plików między hostami " +
                             "-  chcemy udostepnic listę udostępnianych plików");
-                    clientSocket = new Socket("127.0.0.1", 10000);
-                    connection = new Connection(clientSocket);
+                    connection = new Connection("127.0.0.1", 10000);
                     connection.sendMessage(command);
                     response = connection.readMessage();
                     System.out.println(response);
 //                    clientSocket.close();
                     break;
                 case "PULL":
-                    System.out.println("ściągamy z wybranego hosta plik o zadanej nazwie ");;
+                    System.out.println("ściągamy z wybranego hosta plik o zadanej nazwie ");
+                    System.out.print("Podaj nazwe hosta z ktorego ma być pobrany plik: ");
+                    port = userCommand.readLine();
+                    System.out.println(command);
+                    connection = new Connection("127.0.0.1", port);
+                    connection.sendMessage(command);
+                    response = connection.readMessage();
+                    System.out.println(response);
                     break;
                 case "PUSH":
-                    System.out.println("wrzucamy na wybrany host pliku o zadanej nazwie");;
+                    System.out.println("wrzucamy na wybrany host pliku o zadanej nazwie");
+                    System.out.print("Podaj nazwe hosta do ktorego ma być wysłany plik: ");
+                    port = userCommand.readLine();
+                    System.out.println(command);
+                    connection = new Connection("127.0.0.1", port);
+                    connection.sendMessage(command);
+                    response = connection.readMessage();
+                    System.out.println(response);
                     break;
                 case "QUIT":
                     return;
