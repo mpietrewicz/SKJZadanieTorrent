9b22a0791b6f77e12e4102e1b5d5b06a4f6ff314
Marek <mpietrewicz@gmail.com>
2018-01-02 21:32:58 +0100

    Refaktoryzacja kodu – dodanie klasy Client zastępującą klasę Host oraz implementacja nowej klasy Host zawierającą dwa wątki (Client, Server)

---
+++ src/Client.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Client implements Runnable {
+    int id;
+
+    public Client(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        Connection connection;
+        String port;
+
+        BufferedReader userCommand = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            try {
+                String command = null;
+                command = userCommand.readLine();
+
+                switch (command) {
+                    case "GET":
+                        System.out.println("Wymiana list udostępnianych plików między hostami " +
+                                "-  chcemy wiedzieć gdzie (na jakim hoście) jakie pliki się znajdują, wraz z ich sumami kontrolnymi MD5");
+                        port = "10000";
+                        Thread setRequestThread = new Thread(new SetRequest(command, port));
+                        setRequestThread.start();
+                        break;
+                    case "SET":
+                        System.out.println("Wymiana list udostępnianych plików między hostami " +
+                                "-  chcemy udostepnic listę udostępnianych plików");
+                        port = "10000";
+                        Thread getRequestThread = new Thread(new GetRequest(command, port));
+                        getRequestThread.start();
+                        break;
+                    case "PULL":
+                        System.out.println("ściągamy z wybranego hosta plik o zadanej nazwie ");
+                        System.out.print("Podaj nazwe hosta z ktorego ma być pobrany plik: ");
+                        port = userCommand.readLine();
+                        Thread pullRequestThread = new Thread(new PullRequest(command, port));
+                        pullRequestThread.start();
+                        break;
+                    case "PUSH":
+                        System.out.println("wrzucamy na wybrany host pliku o zadanej nazwie");
+                        System.out.print("Podaj nazwe hosta do ktorego ma być wysłany plik: ");
+                        port = userCommand.readLine();
+                        Thread pushRequestThread = new Thread(new PushRequest(command, port));
+                        pushRequestThread.start();
+                        break;
+                    case "QUIT":
+                        return;
+                    default:
+                        System.out.println("UNRECOGNIZED COMMAND");
+                        ;
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/Host.java
+++ src/Host.java
@@ -1,19 +1,14 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
 
 public class Host {
     public static void main(String[] args) throws IOException {
         int id = Integer.parseInt(args[0]);
-        Connection connection;
-        String port;
 
         if(Connection.isPortValid(10000+id)) {
             System.out.println("Starting host: "+id);
         }
         else {
-            System.out.println("Host id must be in range: 1-99");
+            System.out.println("Client id must be in range: 1-99");
             return;
         }
 
@@ -21,45 +16,8 @@ public class Host {
         Thread serverThread = new Thread(server);
         serverThread.start();
 
-        BufferedReader userCommand = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
-            String command = userCommand.readLine();
-
-            switch (command) {
-                case "GET":
-                    System.out.println("Wymiana list udostępnianych plików między hostami " +
-                            "-  chcemy wiedzieć gdzie (na jakim hoście) jakie pliki się znajdują, wraz z ich sumami kontrolnymi MD5");
-                    port = "10000";
-                    Thread setRequestThread = new Thread(new SetRequest(command, port));
-                    setRequestThread.start();
-                    break;
-                case "SET":
-                    System.out.println("Wymiana list udostępnianych plików między hostami " +
-                            "-  chcemy udostepnic listę udostępnianych plików");
-                    port = "10000";
-                    Thread getRequestThread = new Thread(new GetRequest(command, port));
-                    getRequestThread.start();
-                    break;
-                case "PULL":
-                    System.out.println("ściągamy z wybranego hosta plik o zadanej nazwie ");
-                    System.out.print("Podaj nazwe hosta z ktorego ma być pobrany plik: ");
-                    port = userCommand.readLine();
-                    Thread pullRequestThread = new Thread(new PullRequest(command, port));
-                    pullRequestThread.start();
-                    break;
-                case "PUSH":
-                    System.out.println("wrzucamy na wybrany host pliku o zadanej nazwie");
-                    System.out.print("Podaj nazwe hosta do ktorego ma być wysłany plik: ");
-                    port = userCommand.readLine();
-                    Thread pushRequestThread = new Thread(new PushRequest(command, port));
-                    pushRequestThread.start();
-                    break;
-                case "QUIT":
-                    return;
-                default:
-                    System.out.println("UNRECOGNIZED COMMAND");;
-                    break;
-            }
-        }
+        Client client = new Client(id);
+        Thread clientThread = new Thread(client);
+        clientThread.start();
     }
 }
